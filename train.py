4e552388d542f422941f08011eb19ad413336208
Louis Luk <failuk333@hotmail.com>
2020-06-01 01:01:02 +0800

    edit some part in train

--- train.py
+++ train.py
@@ -1,16 +1,27 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
+from model.model import BERT
+from process.process import Processing
+
 
 def main():
     # Load data
     train_path = "./data/train_file.csv"
-    test_path = "./data/test_file.csv"
+    #test_path = "./data/test_file.csv"
 
     train_data = pd.read_csv(train_path)
-    test_data = pd.read_csv(test_path)
+    #test_data = pd.read_csv(test_path)
 
     train, val = train_test_split(train_data, test_size=0.2, random_state=420)
+
+    max_length = 128
+    bert = BERT()
+
+    train_example = Processing.to_bert_example(train, "Title", "Headline", "Sentiment")
+
+
+
 
 
 
