ab00c759c6db76900cf00487a98ebb20ec9e6de4
Louis Luk <failuk333@hotmail.com>
2020-06-02 00:05:37 +0800

    change to train 2 models

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ def main():
     train_path = "./data/train_file.csv"
     train_data = pd.read_csv(train_path)
 
-    train_data = Cleansing.run_clean(train_data)[:100]
+    train_data = Cleansing.run_clean(train_data)
 
     max_length = 128
     num_labels = 3
@@ -20,9 +20,14 @@ def main():
     model.summary()
 
     train_example = Processing.to_bert_input(bert.tokenizer, train_data, "Title", "Headline", max_length)
-    train_y = Cleansing.get_y(train_data, "SentimentTitle")
+    train_y_t = Cleansing.get_y(train_data, "SentimentTitle")
+    train_y_hl = Cleansing.get_y(train_data, "SentimentHeadline")
 
-    model.fit(train_example, train_y, epochs=30, batch_size=32, validation_split=0.2, shuffle=True)
+    model.fit(train_example, train_y_t, epochs=30, batch_size=32, validation_split=0.2, shuffle=True)
+    model.save_weights('./model/model_title.h5')
+
+    model.fit(train_example, train_y_hl, epochs=30, batch_size=32, validation_split=0.2, shuffle=True)
+    model.save_weights('./model/model_headline.h5')
 
 
 if __name__ == '__main__':
